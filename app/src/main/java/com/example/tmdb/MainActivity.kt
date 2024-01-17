8459d2cbd1174b83eec07663d414bf7295597b6a
huuphuoc1396 <huuphuoc1396@gmail.com>
2024-01-17 15:39:15 +0700

    Implement app module: Setup app fullscreen

--- app/src/main/java/com/example/tmdb/MainActivity.kt
+++ app/src/main/java/com/example/tmdb/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.tmdb
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -13,15 +17,17 @@ import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                ) { innerPadding ->
+                    Greeting("Android", modifier = Modifier
+                        .systemBarsPadding()
+                        .padding(innerPadding))
                 }
             }
         }
